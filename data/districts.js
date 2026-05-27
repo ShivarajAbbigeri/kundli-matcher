45ead920540223aa990e195390d6174d0f122b5a
Shivaraj Abbigeri <shivarajabbigeri@gmail.com>
2026-05-28 01:50:01 +0530

    Create districts.js

---
+++ data/districts.js
@@ -0,0 +1,137 @@
+const KARNATAKA_DISTRICTS = [
+    {
+        name: "Bagalkot",
+        lat: 16.1800,
+        lon: 75.6900
+    },
+    {
+        name: "Ballari",
+        lat: 15.1394,
+        lon: 76.9214
+    },
+    {
+        name: "Belagavi",
+        lat: 15.8497,
+        lon: 74.4977
+    },
+    {
+        name: "Bengaluru Urban",
+        lat: 12.9716,
+        lon: 77.5946
+    },
+    {
+        name: "Bidar",
+        lat: 17.9133,
+        lon: 77.5301
+    },
+    {
+        name: "Chamarajanagar",
+        lat: 11.9231,
+        lon: 76.9395
+    },
+    {
+        name: "Chikkaballapur",
+        lat: 13.4350,
+        lon: 77.7315
+    },
+    {
+        name: "Chikkamagaluru",
+        lat: 13.3153,
+        lon: 75.7754
+    },
+    {
+        name: "Davanagere",
+        lat: 14.4644,
+        lon: 75.9218
+    },
+    {
+        name: "Dharwad",
+        lat: 15.4589,
+        lon: 75.0078
+    },
+    {
+        name: "Gadag",
+        lat: 15.4315,
+        lon: 75.6355
+    },
+    {
+        name: "Hassan",
+        lat: 13.0072,
+        lon: 76.0962
+    },
+    {
+        name: "Haveri",
+        lat: 14.7937,
+        lon: 75.4041
+    },
+    {
+        name: "Hubballi",
+        lat: 15.3647,
+        lon: 75.1240
+    },
+    {
+        name: "Kalaburagi",
+        lat: 17.3297,
+        lon: 76.8343
+    },
+    {
+        name: "Kodagu",
+        lat: 12.4244,
+        lon: 75.7382
+    },
+    {
+        name: "Kolar",
+        lat: 13.1377,
+        lon: 78.1299
+    },
+    {
+        name: "Koppal",
+        lat: 15.3450,
+        lon: 76.1548
+    },
+    {
+        name: "Mandya",
+        lat: 12.5239,
+        lon: 76.8974
+    },
+    {
+        name: "Mangaluru",
+        lat: 12.9141,
+        lon: 74.8560
+    },
+    {
+        name: "Mysuru",
+        lat: 12.2958,
+        lon: 76.6394
+    },
+    {
+        name: "Raichur",
+        lat: 16.2076,
+        lon: 77.3463
+    },
+    {
+        name: "Shivamogga",
+        lat: 13.9299,
+        lon: 75.5681
+    },
+    {
+        name: "Tumakuru",
+        lat: 13.3409,
+        lon: 77.1010
+    },
+    {
+        name: "Udupi",
+        lat: 13.3409,
+        lon: 74.7421
+    },
+    {
+        name: "Vijayapura",
+        lat: 16.8302,
+        lon: 75.7100
+    },
+    {
+        name: "Yadgir",
+        lat: 16.7704,
+        lon: 77.1376
+    }
+];
